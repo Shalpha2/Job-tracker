1b8a861cb5e17e30fd6d945818cbac7abc9c131b
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-03-25 01:32:44 -0700

    fix

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const applicationsList=document.getElementById("#applications-list")
+    console.log(applicationsList)
     const form =document.querySelector("form#job-form")
-    console.log(form)
+   // console.log(form)
     form.addEventListener("submit", handleclick);
      
     function handleclick(event){
         event.preventDefault()
         const jobForm ={
-jobtitle:event.target.jobtitle,value
+jobtitle:event.target.jobtitle.value,
+company:event.target.copmany.value,
+status:event.target.status.value,
+
         }
     }
     
