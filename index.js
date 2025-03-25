d8ccdddc153698aa81f4f467e20dc04677de2177
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-03-25 04:38:49 -0700

    fix

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const applicationsList=document.getElementById("#applications-list")
-    console.log(applicationsList)
-    const form =document.querySelector("form#job-form")
-   // console.log(form)
-    form.addEventListener("submit", handleclick);
-     
-    function handleclick(event){
-        event.preventDefault()
-        const jobForm ={
-jobtitle:event.target.jobtitle.value,
-company:event.target.copmany.value,
-status:event.target.status.value,
-
-        }
+document.addEventListener("DOMContentLoaded", function () {
+    const applicationsList = document.getElementById("applications-list");
+    const form = document.querySelector("form#job-form");
+ // console.log(form)
+    form.addEventListener("submit", handleSubmit);
+  
+    fetch("http://localhost:3000/applications")
+      .then((response) => response.json())
+      .then((applications) => {
+        renderapplication(applications);
+      });
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+  
+      const newApplication = {
+        jobtitle: event.target.jobTitle.value,
+        company: event.target.company.value,
+        status: event.target.status.value,
+      };
+  
+      
+      fetch("http://localhost:3000/applications", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newApplication),
+      })
+        .then((response) => response.json())
+        .then((app) => {
+          renderapplication(app); 
+          form.reset(); 
+        });
     }
-    
-})
+  
+    function renderapplication(applications) {
+      applications.forEach((app) => {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button")
+        li.innerHTML = `
+          <strong>${app.jobtitle}</strong> at ${app.company} — <em>${app.status}</em>
+        `;
+        li.appendChild(deleteButton),
+        applicationsList.appendChild(li);
+      });
+    }
+   
+  });
+
+  
